edefbbfeb5f96fa7678c252726e8b21325d2c236
NorthSouLI <32359055+NorthSouLI@users.noreply.github.com>
2018-02-23 15:09:46 +0300

    Add files via upload

--- soru1.java
+++ soru1.java
@@ -1,4 +1,4 @@
-package ödev;
+package ödev2;
 
 import java.util.Scanner;
 
@@ -7,28 +7,42 @@ public class soru1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner giris= new Scanner(System.in);
-		
-		int a, b , c , d, e;
-		double f,t,y,u;
-		
-		System.out.println("A deðerini giriniz :");
-		a= giris.nextInt();
-		System.out.println("B deðerini giriniz :");
-		b= giris.nextInt();
-		System.out.println("C deðerini giriniz :");
-		c= giris.nextInt();
-		System.out.println("D deðerini giriniz :");
-		d= giris.nextInt();
-		System.out.println("E deðerini giriniz :");
-		e= giris.nextInt();
-		
-		
-		t=((a+(b*c))/(d*e));
-		y=(a/c)*b;
-		u=(((a*b)*(a*b))/(c+d));
-		f=t+y+u;
-		System.out.println("F ="+f);
+		Scanner giris=new Scanner(System.in);
+        int yas,buyukyas =0,buyukyasort=0,kýrkustuort=0,kýrkaltýort=0,kucukyasort=0,kucukyas =0,maas,limit,maasort=0,yasort=0,yastoplam=0,maastoplam=0;
+        System.out.print("Kac kisi giriceksiniz =");
+        limit=giris.nextInt();
+        Byte s;
+        for (s=0;s<limit;s++){   
+        
+        System.out.println("Yasinizi giriniz");
+        yas=giris.nextInt();
+        System.out.println("Maasinizi giriniz");
+        maas=giris.nextInt();
+		yastoplam+=yas;
+		maastoplam+=maas;
+		yasort =yastoplam/limit;
+		maasort =maastoplam/limit;
+		if (yas<=39){
+		 kucukyas+=yas;  
+		 kucukyasort=kucukyas/yas;
+		 kýrkaltýort=kucukyas/kucukyasort;
+		 } else {
+		 
+		 buyukyas+=yas;
+		 buyukyasort=buyukyas/yas;
+		 kýrkustuort=buyukyas/buyukyasort;
+ 	}
+ 	}
+ 	System.out.println("Yas ortalamasi :"+yasort);
+ 	System.out.println("Maas ortalamasi :"+maasort);
+ 	System.out.println("Yas toplami :"+yastoplam);
+ 	System.out.println("Maas toplami :"+maastoplam);
+ 	System.out.println("40 yas ustu toplamý :"+buyukyas);
+ 	System.out.println("40 yas altý toplamý :"+kucukyas);
+ 	System.out.println("40 yas ustu kisi sayisi :"+buyukyasort);
+ 	System.out.println("40 yas altý kisi sayisi :"+kucukyasort);
+ 	System.out.println("40 yas altý ortalama :"+kýrkaltýort);
+ 	System.out.println("40 yas ustu ortalama :"+kýrkustuort);
 	}
 
 }
--- soru2.java
+++ soru2.java
@@ -1,4 +1,4 @@
-package ödev;
+package ödev2;
 
 import java.util.Scanner;
 
@@ -8,28 +8,32 @@ public class soru2 {
 		// TODO Auto-generated method stub
 
 		Scanner giris=new Scanner(System.in);
+		byte limit,sayý,psayý =0,nsayý=0,pkisi=0,nkisi=0,port=0,nort=0;
 		
+		System.out.println("Limit deðerini giriniz :");
+		limit=giris.nextByte();
+		byte i;
 		
-		double m,km,hm,dam,f,dm,cm,mm;
+		for (i=0;i<limit;i++) {
+		System.out.println("Bir sayý giriniz :");	
+		sayý=giris.nextByte();
 		
-		System.out.println("Metre Deðerini giriniz :");
-		m=giris.nextDouble();
-		dam=m/10;
-		hm=m/100;
-		km=m/1000;
-		dm=m*10;
-		cm=m*100;
-		mm=m*1000;
-		
-		
-		System.out.println("Kilometre"+km+
-				"\nHektometre"+hm+
-				"\nDesimetre"+dam+
-				"\nDekametre"+dm+
-				"\nSantimetre"+cm+
-				"\nMilimetre"+mm);
-		
+		if (sayý<0){
+			nkisi++;
+			nsayý+=sayý;
+			nort=(byte) (nsayý/nkisi);
+		} else {
+			pkisi++;
+			psayý+=sayý;
+			port=(byte) (psayý/pkisi);
+		}		
+		}
+		System.out.println("Pozitif sayýlarýn toplamý :"+psayý);
+		System.out.println("Pozitif sayýlarýn adedi :"+pkisi);
+		System.out.println("Pozitif sayýlarýn ortalamasý :"+port);
+		System.out.println("Negatif sayýlarýn toplamý :"+nsayý);
+		System.out.println("Negatif sayýlarýn adedi :"+nkisi);
+		System.out.println("Negatif sayýlarýn ortalamasý :"+nort);
 		
 	}
-
 }
